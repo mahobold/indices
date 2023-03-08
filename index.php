a132e303f90e7396d2cb28f830fd20ad2eaa7a56
MARIA VITORIA MAGALHAES HOBOLD <hobold.7994@aluno.pr.senac.br>
2023-03-08 11:56:10 -0300

    indices

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    $pessoa1 = array();
+    $pessoa2 = array();
+    $pessoa3 = array();
+    $pessoa4 = array();
+
+    $pessoa1 [0][0][0][0][0]["nome"] = "Marcela";
+    $pessoa1 [0][0][0][0][0]["idade"] = "15";
+    $pessoa1 [0][0][0][0][0]["aniversário"] = "26/05";
+    $pessoa1 [0][0][0][0][0]["signo"] = "Gêmeos";
+
+    $pessoa2 [0][0][0][0][0]["nome"] = "Wagatuma";
+    $pessoa2 [0][0][0][0][0]["idade"] = "16";
+    $pessoa2 [0][0][0][0][0]["aniversário"] = "20/07";
+    $pessoa2 [0][0][0][0][0]["signo"] = "Cãncer";
+
+    $pessoa3 [0][0][0][0][0]["nome"] = "Ana";
+    $pessoa3 [0][0][0][0][0]["idade"] = "16";
+    $pessoa3 [0][0][0][0][0]["aniversário"] = "26/02";
+    $pessoa3 [0][0][0][0][0]["signo"] = "Peixes";
+
+    $pessoa4 [0][0][0][0][0]["nome"] = "Letícia";
+    $pessoa4 [0][0][0][0][0]["idade"] = "16";
+    $pessoa4 [0][0][0][0][0]["aniversário"] = "22/05";
+    $pessoa4 [0][0][0][0][0]["signo"] = "Gêmeos";
+
+    echo $pessoa1 [0][0][0][0][0]["nome"] . " tem " . $pessoa1[0][0][0][0][0]["idade"] . " anos.";
+    echo ("</br>");
+    echo $pessoa2 [0][0][0][0][0]["nome"] . " tem " . $pessoa2[0][0][0][0][0]["idade"] . " anos.";
+    echo ("</br>");
+    echo $pessoa3 [0][0][0][0][0]["nome"] . " tem " . $pessoa3[0][0][0][0][0]["idade"] . " anos.";
+    echo ("</br>");
+    echo $pessoa4 [0][0][0][0][0]["nome"] . " tem " . $pessoa4[0][0][0][0][0]["idade"] . " anos.";
+    
+
+?>
+
